754f9e84364e1e91eb4a740609d7c780337e3728
Rich-McGrath <richmcgrath@gmail.com>
2016-05-19 16:39:18 -0400

    Fixed typo in test relating to page title

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -4,6 +4,6 @@ describe 'Creating todo lists' do
   it 'redirects to the todo list index page on success' do
     visit '/todo_lists'
     click_link 'New Todo list'
-    expect(page).to have_content('New todo_lists')
+    expect(page).to have_content('New Todo List')
   end
 end
