ba4fd494ac3c905d930485addd7a99437939edfb
Rich-McGrath <richmcgrath@gmail.com>
2016-05-24 22:08:06 -0400

    Added has many association for todo item model

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe TodoList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe TodoList, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe TodoList do
+  it { should have_many(:todo_items) }
 end
