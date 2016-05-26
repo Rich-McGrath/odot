5e85f6b333aa0f4c0c50bccf67d94e237451eb75
Rich-McGrath <richmcgrath@gmail.com>
2016-05-26 14:23:20 -0400

    Todo Item validations working

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,5 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
-  validates :content, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 2 }
 end
